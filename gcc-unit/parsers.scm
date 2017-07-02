0686eff12693ed5b6d667bdd6a0346543b74c48b
Danny Milosavljevic <dannym@scratchpost.org>
2017-07-02 22:53:47 +0200

    parsers: Deserialize some more.

--- gcc-unit/parsers.scm
+++ gcc-unit/parsers.scm
@@ -27,14 +27,28 @@
    (values ;(value nonref-values) : (string-append $1 " " $2) ; special-case "strg: foo bar baz lngt: 11" - which is really a stupid way to write it but hey.
            (@ id) : (reference $2)
            (value) : $1)
-   (attribute (name : values) : (list $1 $3))))
+   (attribute (name : values) : (cons $1 $3))))
 
 ; TODO eval the result, resolving "reference"s.
+
+(define sentinel (gensym))
+(define (hash-ref-or-die hash-table key err)
+    (let ((result (hash-ref hash-table key sentinel)))
+      (if (eq? result sentinel)
+        (err key)
+        result)))
+
+(define* (create-record-instance-by-lists proc attribute-names attribute-definitions #:optional (required-attribute-names attribute-names))
+  (let* ((attribute-definitions (alist->hash-table attribute-definitions)))
+      (write "X")
+    (write (hash-map->list cons attribute-definitions))
+  (let ((constructor-args (map (cut hash-ref-or-die attribute-definitions <> (lambda (key) (if (member key required-attribute-names) (error key) #f))) attribute-names)))
+    (apply proc constructor-args))))
 
 (define (create-record-instance id type-name attributes)
   (cons id
     (cond
-      ((eq? type-name 'type_decl) (type-decl "NAME" "TYPE" "SCPE" "SRCP" "CHAIN"))
+      ((eq? type-name 'type_decl) (create-record-instance-by-lists type-decl '(name type scpe srcp chain) attributes '(name type chain)))
       (else (cons type-name attributes)))))
 
 (define* (parse port #:optional (definition-creator create-record-instance))
