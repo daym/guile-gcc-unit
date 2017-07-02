dc6a97230dc6a9017a2258b252ad8e9366b018de
Danny Milosavljevic <dannym@scratchpost.org>
2017-07-02 21:29:44 +0200

    Make main more interesting.

--- main.scm
+++ main.scm
@@ -1,5 +1,15 @@
 (use-modules (gcc-unit parsers))
+(use-modules (ice-9 match))
 
-(write (call-with-input-string "
+(define f (call-with-input-string "
 @1   type_decl   name: @2 lngt: 2
 @2  identifier_node strg: int   lngt: 3" parse))
+
+(define (visit node)
+  (write node)
+  (match node
+    ((_ 'type-decl . args) (write " TYPE "))
+    (_ #f))
+  (newline))
+
+(map visit f)
