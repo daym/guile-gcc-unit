a544f6956e39757b9f3c1ff4f56ce4aaa4386eb1
Danny Milosavljevic <dannym@scratchpost.org>
2017-07-02 23:45:37 +0200

    main: Match some nodes manually.

--- main.scm
+++ main.scm
@@ -8,6 +8,11 @@
 (define f (call-with-input-string "
 @1   type_decl   name: @2 type: 2  chain: 4
 @2  identifier_node strg: int   lngt: 3" (cut parse <> create-instance)))
-(for-each (lambda (entry)
-            (write entry)
+
+(for-each (lambda (node)
+            (match node
+              (('identifier_node attributes)
+                (write "ID"))
+              (_ #f))
+            (write node)
             (newline)) f)
