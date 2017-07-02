613788307d2fc43d8753892a0c10ed704b44ce13
Danny Milosavljevic <dannym@scratchpost.org>
2017-07-02 22:24:57 +0200

    parsers: Move definition-creator to entry point.

--- gcc-unit/parsers.scm
+++ gcc-unit/parsers.scm
@@ -4,18 +4,19 @@
   #:use-module (gcc-unit lexers)
   #:use-module (ice-9 match)
   #:use-module (ice-9 hash-table)
+  #:use-module (srfi srfi-26) ; cut
   #:export (parse))
 
 (define (reference value)
   `(reference ,value))
 
-(define (make-parser definition-creator)
+(define (make-parser)
   (lalr-parser
    (def @ value :) ; terminals
    (program (definition program) : (cons $1 $2)
             (*eoi*) : '())
-   (definition (def @ id type-name attributes) : (definition-creator $3 $4 $5)
-               (def @ id type-name) : (definition-creator $3 $4 '()))
+   (definition (def @ id type-name attributes) : (list $3 $4 $5)
+               (def @ id type-name) : (list $3 $4 '()))
    (attributes (attribute attributes) : (cons $1 $2)
                (attribute) : (cons $1 '()))
    (type-name (value) : (string->symbol $1)) ; At the meta level.
@@ -37,7 +38,8 @@
       (else (cons type-name attributes)))))
 
 (define* (parse port #:optional (definition-creator create-record-instance))
-  (let* ((entries ((make-parser definition-creator) (make-lexer port) error))
+  (let* ((entries ((make-parser) (make-lexer port) error))
+         (entries (map (cut apply definition-creator <>) entries))
          (result (alist->hash-table entries)))
     ;(write (hash-ref result (string->symbol "1")))
     (hash-map->list cons result)))
