41736053cf3c68056c88a3a2611b29d0af61bd5a
Danny Milosavljevic <dannym@scratchpost.org>
2017-07-02 23:12:08 +0200

    main: Clean up.

--- main.scm
+++ main.scm
@@ -2,14 +2,7 @@
 (use-modules (ice-9 match))
 
 (define f (call-with-input-string "
-@1   type_decl   name: @2 lngt: 2
+@1   type_decl   name: @2 type: 2  chain: 4
 @2  identifier_node strg: int   lngt: 3" parse))
-
-(define (visit node)
-  (write node)
-  (match node
-    ((_ 'type-decl . args) (write " TYPE "))
-    (_ #f))
-  (newline))
-
-(map visit f)
+;(lambda (port) (parse port visit))))
+(write f)
