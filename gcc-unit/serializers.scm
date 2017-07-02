7bb9da5a8ac369fb2c17e9fd18f1aec18acf8508
Danny Milosavljevic <dannym@scratchpost.org>
2017-07-02 23:11:30 +0200

    serializers: Implement name mangling.

--- gcc-unit/serializers.scm
+++ gcc-unit/serializers.scm
@@ -1,20 +1,28 @@
 (define-module (gcc-unit serializers)
   #:use-module (gcc-unit records)
   #:use-module (ice-9 hash-table)
+  #:use-module (ice-9 regex)
   #:use-module (srfi srfi-26)) ; cut
 
 (define sentinel (gensym))
-(define (hash-ref-or-die hash-table key err)
-    (let ((result (hash-ref hash-table key sentinel)))
-      (if (eq? result sentinel)
-        (err key)
-        result)))
+
+(define (mangle-name name)
+  (string->symbol (regexp-substitute/global #f "([A-Za-z])_" (symbol->string name) 'pre 1 'post)))
+
+(define (hash-ref-mangle-or-die hash-table key err)
+  "Mangles KEY.  Looks that up in HASH-TABLE.  If that's not there, calls ERR with the mangled KEY."
+  (let ((result (hash-ref hash-table (mangle-name key) sentinel)))
+    (if (eq? result sentinel)
+      (err (mangle-name key))
+      result)))
 
 (define* (create-record-instance-by-lists proc attribute-names attribute-definitions #:optional (required-attribute-names attribute-names))
+  "ATTRIBUTE-NAMES and REQUIRED-ATTRIBUTE-NAMES are mangled.  ATTRIBUTE-DEFINITIONS is not (yet)."
   (let* ((attribute-definitions (alist->hash-table attribute-definitions)))
       (write "X")
     (write (hash-map->list cons attribute-definitions))
-  (let ((constructor-args (map (cut hash-ref-or-die attribute-definitions <> (lambda (key) (if (member key required-attribute-names) (error key) #f))) attribute-names)))
+  (let ((constructor-args (map (cut hash-ref-mangle-or-die attribute-definitions <> (lambda (key) (if (member key required-attribute-names) (error key) #f)))
+                               attribute-names)))
     (apply proc constructor-args))))
 
 (define-public (deserialize-record-instance type-name attributes)
