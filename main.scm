25601abcadcf0528e7286807ddf1e67f05583569
Danny Milosavljevic <dannym@scratchpost.org>
2017-07-03 03:00:44 +0200

    main: Actually decode parameters a little.

--- main.scm
+++ main.scm
@@ -3,6 +3,7 @@
 
 (use-modules (gcc-unit parsers))
 (use-modules (ice-9 match))
+(use-modules (ice-9 pretty-print))
 (use-modules (srfi srfi-26)) ; cut
 
 (define (create-instance type-name attributes)
@@ -15,10 +16,52 @@
 
 (define f (call-with-input-file input-file-name (cut parse <> create-instance)))
 
+(define (decode-name node)
+  (match node
+    (('identifier_node attributes)
+     (assoc-ref attributes 'strg))))
+
+(define (decode-basic-type type-node)
+  (match type-node
+    ; TODO function type
+    (('pointer_type attributes)
+     "pointer")
+    (('void_type attributes)
+     "void")
+    (('integer_type attributes)
+     "integer")
+    (('real_type attributes)
+     "real")
+    (('boolean_type attributes)
+     "bool")
+    (('enumeral_type attributes)
+     "enum")))
+
+(define (decode-prms prms)
+  (if prms
+    (match prms
+      (('tree_list attributes)
+       (cons (decode-basic-type (assoc-ref attributes 'valu))
+             (decode-prms (assoc-ref attributes 'chan)))))
+    '())) ; Not really.  void foo() means UNSPECIFIED parameter list.
+
+
 (for-each (lambda (node)
             (match node
-              (('identifier_node attributes)
-                (write "ID"))
+              (('function_decl attributes)
+                ; Note: body == "undefined"
+                 (let* ((name (decode-name (assoc-ref attributes 'name)))
+                        (link (assoc-ref attributes 'link))
+                        (type-node (assoc-ref attributes 'type)))
+                   (if (and (string-prefix? "ped_" name) (string=? "extern" link))
+                          (match type-node
+                            (('function_type type-attributes)
+                              (let* ((type-retn (decode-basic-type (assoc-ref type-attributes 'retn)))
+                                     (type-prms (decode-prms (assoc-ref type-attributes 'prms))))
+                           (write name)
+                           (write type-prms)
+                           (write "->")
+                           (write type-retn)
+                           (newline)))))))
               (_ #f))
-            (write node)
-            (newline)) f)
+            ) f)
